3701c14583be7ad88d8109d2202cc4d35a4b2fc0
Lin Yang <geekya215@gmail.com>
2023-04-15 13:46:51 +0800

    initial project

---
+++ src/main/java/io/geekya215/lamination/Main.java
@@ -0,0 +1,10 @@
+package io.geekya215.lamination;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello lamination!");
+        Byte a = Byte.valueOf(Byte.MAX_VALUE);
+        Byte b = Byte.valueOf(Byte.MAX_VALUE);
+        System.out.println(a == b);
+    }
+}
